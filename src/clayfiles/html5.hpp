0bde6a320c23fe4106005711720c3ec22e591b16
Tristan Isham <tristan@isham.co>
2021-10-05 01:17:20 -0400

    Updating naming

--- src/clayfiles/html5.hpp
+++ src/clayfiles/html5.hpp
@@ -16,28 +16,37 @@ namespace kiln {
 		}
 
 		void parse() {
-			std::cout << "Made it to here!" << std::endl;
-			DataSlice layout_slice;
-
+			DataSlice layout_slice = { 0,"",0 };
+			std::cout << layout_slice.start_line << layout_slice.end_line << std::endl;
 			for (auto line : this->doc) {
 				auto words = Operators::tokenize(line.body, ' ');
-								
-				if (words[0] == "~layout~" && words.size() <= 1) {
-					layout_slice.start_line = line.num;
+
+				if (layout_slice.start_line != NULL) {
+					
 				}
-				else if (words[0] == "~/layout~" && words.size() <= 1) {
-					if (layout_slice.start_line != NULL) {
-						layout_slice.end_line = line.num;
+				else {
+					
+					if (words[0] == "~layout~" && words.size() <= 1) {
+						layout_slice.start_line = line.num;
 					}
+					else if (words[0] == "~/layout~" && words.size() <= 1) {
+						if (layout_slice.start_line != NULL) {
+							layout_slice.end_line = line.num;
+						}
+					}
+
+					for (int s = layout_slice.start_line; s < layout_slice.end_line - 1; s++) {
+						layout_slice.body += this->doc[s].body;
+					}
+
+					
 				}
 				
 			}
 
-			for (int s = layout_slice.start_line; s < layout_slice.end_line - 1; s++) {
-				layout_slice.body += this->doc[s].body;
-			}
-
 			std::cout << layout_slice.body << std::endl;
+
+			
 		}
 
 	private:
